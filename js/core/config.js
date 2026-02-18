59e5799b4a12aac1ef4dad42c4c03e566810a122
LordGrimm234 <ryanseacrestjames43@gmail.com>
2026-02-18 16:12:46 +0530

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAv6AEPweeO4ss4fy0o2feHSYBuGCEJOKo",
+  authDomain: "poker-main-c38a0.firebaseapp.com",
+  projectId: "poker-main-c38a0",
+  storageBucket: "poker-main-c38a0.firebasestorage.app",
+  messagingSenderId: "810222880343",
+  appId: "1:810222880343:web:5927471e1843c1c54ec512"
+};
