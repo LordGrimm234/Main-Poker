d366e1cb0edcf9f4d9e21f07c87b6e05474c3a18
LordGrimm234 <ryanseacrestjames43@gmail.com>
2026-02-18 16:14:45 +0530

    Update firebase-init.js

--- js/core/firebase-init.js
+++ js/core/firebase-init.js
@@ -1,1 +1,12 @@
+// js/core/firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { firebaseConfig } from "./config.js";
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export these so other files can use the Database (db) and Login system (auth)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
